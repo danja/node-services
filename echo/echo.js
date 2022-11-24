40499e44133b3330633c13baa0512d4b1cd0daa6
Danny Ayers <danny.ayers@gmail.com>
2022-11-24 16:21:07 +0100

    added CORS headers

--- echo/echo.js
+++ echo/echo.js
@@ -10,14 +10,19 @@ server
       })
       .on('end', () => {
         body = Buffer.concat(body).toString()
+        response.status(200)
+        response.setHeader('Content-Type', 'text/html')
 
-        response.writeHead(200, { 'Content-Type': 'text/html' })
+        /* CORS */
+        response.setHeader('Access-Control-Allow-Origin', '*')
+        response.setHeader('Access-Control-Allow-Methods', 'OPTIONS, GET')
+        response.setHeader('Access-Control-Max-Age', 2592000) // 30 days -
 
         response.write(`${request.method} ${request.url}`)
-        //  response.write(JSON.stringify(request.headers))
-        response.write(toHTML(JSON.parse(JSON.stringify(request.headers))))
 
-        response.write(JSON.stringify(body))
+        response.write(toHTML(JSON.parse(JSON.stringify(request.headers)))) // crazy but true
+
+        // response.write(JSON.stringify(body))
         response.end()
       })
   })
