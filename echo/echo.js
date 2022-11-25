db77694d6477f275c9a2ecef35e8801edda32f64
Danny Ayers <danny.ayers@gmail.com>
2022-11-25 14:35:35 +0100

    Access-Control-Allow-Headers: Accept

--- echo/echo.js
+++ echo/echo.js
@@ -16,15 +16,18 @@ server
         /* CORS */
         response.setHeader('Access-Control-Allow-Origin', '*')
         response.setHeader('Access-Control-Allow-Methods', 'OPTIONS, GET')
-        // response.setHeader('Access-Control-Allow-Headers', 'Content-Type')
+        response.setHeader('Access-Control-Allow-Headers', 'Accept')
         response.setHeader('Access-Control-Max-Age', 2592000) // 30 days -  preflight request cache time
 
-        // hacky
-        if (request.headers.accept.startsWith('application/json')) {
+        // console.log(request.headers.accept)
+        // hacky request.headers.accept.startsWith('application/json')
+        if (request.headers.accept.startsWith('*/*')) {
+          // console.log('json')
           response.setHeader('Content-Type', 'application/json')
           response.write(JSON.stringify(request.headers))
         } else {
           // default to HTML
+          //  console.log('html')
           response.setHeader('Content-Type', 'text/html')
           response.write(`${request.method} ${request.url}`) // for now
           response.write(toHTML(JSON.parse(JSON.stringify(request.headers)))) // crazy but true
